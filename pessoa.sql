bbec7d386e502952fc2060a72ea3e725a390034b
Matheus arantes <m.andrey.a@hotmail.com>
2024-10-31 03:38:46 -0300

    Ajuste-create

--- pessoa.sql
+++ pessoa.sql
@@ -10,5 +10,7 @@ CREATE TABLE `consuta` (
   `endereco` varchar(80) COLLATE utf8_unicode_ci NOT NULL,
   `telefone` varchar(35) COLLATE utf8_unicode_ci NOT NULL,
   `email` varchar(40) COLLATE utf8_unicode_ci NOT NULL,
-  `sexo` varchar(1) COLLATE utf8_unicode_ci NOT NULL
+  `sexo` varchar(1) COLLATE utf8_unicode_ci NOT NULL,
+  `doutor` varchar(1) COLLATE utf8_unicode_ci NOT NULL,
+  `data_hora` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
